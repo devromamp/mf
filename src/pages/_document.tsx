c1f1881cc733d764e2b38f3c393c60f20772353d
roma <roma@mejorapress.com>
2023-10-20 17:06:26 +0100

    project initialize

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap"
+        />
+      </Head>
+      <title>Meet Stefan</title>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
